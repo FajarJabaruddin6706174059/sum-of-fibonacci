710c2c168a2e808b9da30b9204790fad65def06f
Fajar Jabaruddin <48698655+FajarJabaruddin6706174059@users.noreply.github.com>
2019-04-02 10:01:08 +0700

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,18 +6,18 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        Scanner scnr = new Scanner(System.in);
-        int t1 = 1, t2 = 1;
+         Scanner scnr = new Scanner(System.in);
+        int t1 = 1, t2 = 1, t3 = 0, t4 = 2;
         System.out.print("Masukan angka : ");
         int n = scnr.nextInt();
 
+
         for (int i = 1; i <= n; ++i)
         {
-            System.out.print(t1 + " ");
-
-            int sum = t1 + t2;
-            t1 = t2;
-            t2 = sum;
+            t3 = t1 + t2;
+            t4 = t3 + t4;
         }
+
+        System.out.print(t4);
     }
 }
